b195f73d5831bf31fcfe84db06ce6d0bbf6a7e94
Simon Kallmaier <simon.kallmaier@web.de>
2024-05-10 15:33:48 +0200

    Use gpt3 instead of textract queries

--- src/id_scanner.py
+++ src/id_scanner.py
@@ -1,18 +1,6 @@
-import os
+import ast
 
-import boto3
-import pandas as pd
-import streamlit as st
-import trp.trp2 as t2
-from PIL import Image
-
-from utils import TEXTRACT, get_text_information, image_loader
-
-# Set AWS credentials from environment variables
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
-# Configure Boto3 client
+from utils import get_completion, get_text_information
 
 print("Successfully configured Boto3 client.")
 
@@ -21,37 +9,49 @@ def cost_id_scanner():
     return 15 / 1000  # 0.015 USD per page
 
 
-def extract_id_information(image: Image.Image):
-    imageBytes = image_loader(image)
+def extract_id_information(image):
 
-    response = TEXTRACT.detect_document_text(Document={"Bytes": imageBytes})
+    text = get_text_information(image)
+    salary_information = get_completion(text, salary=False)
+    return ast.literal_eval(salary_information)
 
-    # Call Textract
-    response = TEXTRACT.analyze_document(
-        Document={"Bytes": imageBytes},
-        # new QUERIES Feature Type for Textract Queries
-        # We could add additional Feature Types like FORMS and/or TABLES
-        # FeatureTypes=["QUERIES", "FORMS", "TABLS"],
-        FeatureTypes=["QUERIES", "FORMS"],
-        QueriesConfig={
-            "Queries": [
-                {"Text": "What is the value below the Vornamen/Given names/ prenoms?", "Alias": "GIVEN_NAME"},
-                {"Text": "What is the value below Name/Surname/Nom?", "Alias": "SURNAME"},
-                {"Text": "What is the value for date of birth?", "Alias": "DATE_OF_BIRTH"},
-                {"Text": "What is the Nationality?", "Alias": "NATIONALITY"},
-                {"Text": "What is the date of expiry?", "Alias": "DATE_OF_EXPIRY"},
-                {"Text": "What is the place of Birth?", "Alias": "PLACE_OF_BIRTH"},
-                {"Text": "What is the ID Number on the header of the Document?", "Alias": "ID_NUMBER_TOP"},
-                {"Text": "What is the Number on the bottom of the Document?", "Alias": "ID_NUMBER_BOTTOM"},
-            ]
-        },
-    )
 
-    d = t2.TDocumentSchema().load(response)
-    page = d.pages[0]
+# import os
 
-    query_answers = d.get_query_answers(page=page)
+# import pandas as pd
+# import streamlit as st
+# import trp.trp2 as t2
+# from PIL import Image
+# imageBytes = image_loader(image)
 
-    costs = cost_id_scanner()
-    st.write(f"Total cost: ${costs:.4f} USD")
-    return pd.DataFrame(query_answers).rename(columns={0: "Question", 1: "Alias", 2: "Answer"})
+# response = TEXTRACT.detect_document_text(Document={"Bytes": imageBytes})
+
+# # Call Textract
+# response = TEXTRACT.analyze_document(
+#     Document={"Bytes": imageBytes},
+#     # new QUERIES Feature Type for Textract Queries
+#     # We could add additional Feature Types like FORMS and/or TABLES
+#     # FeatureTypes=["QUERIES", "FORMS", "TABLS"],
+#     FeatureTypes=["QUERIES", "FORMS"],
+#     QueriesConfig={
+#         "Queries": [
+#             {"Text": "What is the value below the Vornamen/Given names/ prenoms?", "Alias": "GIVEN_NAME"},
+#             {"Text": "What is the value below Name/Surname/Nom?", "Alias": "SURNAME"},
+#             {"Text": "What is the value for date of birth?", "Alias": "DATE_OF_BIRTH"},
+#             {"Text": "What is the Nationality?", "Alias": "NATIONALITY"},
+#             {"Text": "What is the date of expiry?", "Alias": "DATE_OF_EXPIRY"},
+#             {"Text": "What is the place of Birth?", "Alias": "PLACE_OF_BIRTH"},
+#             {"Text": "What is the ID Number on the header of the Document?", "Alias": "ID_NUMBER_TOP"},
+#             {"Text": "What is the Number on the bottom of the Document?", "Alias": "ID_NUMBER_BOTTOM"},
+#         ]
+#     },
+# )
+
+# d = t2.TDocumentSchema().load(response)
+# page = d.pages[0]
+
+# query_answers = d.get_query_answers(page=page)
+
+# costs = cost_id_scanner()
+# st.write(f"Total cost: ${costs:.4f} USD")
+# return pd.DataFrame(query_answers).rename(columns={0: "Question", 1: "Alias", 2: "Answer"})
